0b27be77c04f563d063cc3b3c67c67901b71571b
tristen s. <78326803+BopeChanUwU@users.noreply.github.com>
2025-10-02 21:54:54 -0700

    feat: update searchbar component layout and styling, integrate into main app structure

--- src/app/Components/searchbar/searchbar.ts
+++ src/app/Components/searchbar/searchbar.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbar } from "@angular/material/toolbar";
 
 
 @Component({
   selector: 'app-searchbar',
-  imports: [MatIconModule, MatFormFieldModule, MatInputModule],
+  imports: [MatIconModule, MatFormFieldModule, MatInputModule, MatToolbar],
   templateUrl: './searchbar.html',
   styleUrl: './searchbar.css'
 })
