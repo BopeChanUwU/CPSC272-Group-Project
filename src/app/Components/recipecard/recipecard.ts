113b8396c2bfccf8f5eda25159267368e7cfaaca
tristen s. <78326803+BopeChanUwU@users.noreply.github.com>
2025-10-08 23:27:15 -0700

    added imports and css to recipecard

--- src/app/Components/recipecard/recipecard.ts
+++ src/app/Components/recipecard/recipecard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from "@angular/material/card";
 import { MatIcon } from "@angular/material/icon";
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-recipecard',
-  imports: [MatCardModule, MatIcon],
+  imports: [MatCardModule, MatIcon, MatButton],
   templateUrl: './recipecard.html',
   styleUrl: './recipecard.css'
 })
