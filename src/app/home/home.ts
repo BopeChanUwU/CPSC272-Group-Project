f0b4a758483b81f7d019afe333cd53aaf5898791
tristen s. <78326803+BopeChanUwU@users.noreply.github.com>
2025-10-10 15:42:59 -0700

    cleaning up unused imports

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,16 +2,12 @@ import { Component, input } from '@angular/core';
 import { MatCardModule } from "@angular/material/card";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { Header } from "../Components/header/header";
-import { MatIcon } from "@angular/material/icon"
-import { MatButton } from "@angular/material/button";
 import { LikeButton } from "../Components/like-button/like-button";
 import { SkipButton } from "../Components/skip-button/skip-button";
-import { Recipecard } from "../Components/recipecard/recipecard";
-
 
 @Component({
   selector: 'app-home',
-  imports: [MatCardModule, MatToolbarModule, Header, MatIcon, MatButton, LikeButton, SkipButton, Recipecard],
+  imports: [MatCardModule, MatToolbarModule, Header, LikeButton, SkipButton],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
