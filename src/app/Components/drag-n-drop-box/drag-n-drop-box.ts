41d54803cc3e5c3828820c3e03120e5153b6989d
tristen s. <78326803+BopeChanUwU@users.noreply.github.com>
2025-10-13 12:17:54 -0700

    create page is a little broken right now working on it

--- src/app/Components/drag-n-drop-box/drag-n-drop-box.ts
+++ src/app/Components/drag-n-drop-box/drag-n-drop-box.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class DragNDropBox {
 
+  /* "use strict";
+  function dragNdrop(event) {
+    var fileName = URL.createObjectURL(event.target.files[0]);
+    var preview = document.getElementById("preview");
+    var previewImg = document.createElement("img");
+    previewImg.setAttribute("src", fileName);
+    preview.innerHTML = "";
+    preview.appendChild(previewImg);
+  } 
+  function drag() {
+    document.getElementById('uploadFile').parentNode.className = 'draging dragBox';
+  }
+  function drop() {
+    document.getElementById('uploadFile').parentNode.className = 'dragBox';
+  } */
 }
--- src/app/profile/createrecipe/createrecipe.ts
+++ src/app/profile/createrecipe/createrecipe.ts
@@ -5,10 +5,15 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 import { CreateButton } from "../../Components/create-button/create-button";
+import { MatFormField } from  "@angular/material/form-field";
+import { TitleTextfield } from "../../Components/title-textfield/title-textfield";
+import { MatInputModule } from '@angular/material/input';
+import { DescriptionTextfield } from "../../Components/description-textfield/description-textfield";
+
 
 @Component({
   selector: 'app-createrecipe',
-  imports: [Header, Sidebar, MatToolbarModule, MatCardModule, MatIconModule, CreateButton],
+  imports: [Header, Sidebar, MatToolbarModule, MatCardModule, MatIconModule, CreateButton, MatFormField, TitleTextfield, MatInputModule, DescriptionTextfield],
   templateUrl: './createrecipe.html',
   styleUrl: './createrecipe.css'
 })
