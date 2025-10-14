42b986416faf47578428271f9cbecca69f47d66f
tristen s. <78326803+BopeChanUwU@users.noreply.github.com>
2025-10-13 22:38:17 -0700

    feat: add DeleteButton component with styles and functionality made my recipe page look like it will in finished build (delete button isnt centered yet tho) updated about page

--- src/app/profile/myrecipes/myrecipes.ts
+++ src/app/profile/myrecipes/myrecipes.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Sidebar } from "../../Components/sidebar/sidebar";
+import { Header } from "../../Components/header/header";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { Searchbar } from "../../Components/searchbar/searchbar";
+import { Recipecard } from "../../Components/recipecard/recipecard";
+import { DeleteButton } from "../../Components/delete-button/delete-button";
 
 @Component({
   selector: 'app-myrecipes',
-  imports: [],
+  imports: [Sidebar, Header, MatToolbarModule, Searchbar, Recipecard, DeleteButton],
   templateUrl: './myrecipes.html',
   styleUrl: './myrecipes.css'
 })
