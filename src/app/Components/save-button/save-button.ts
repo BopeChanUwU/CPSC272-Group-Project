08850ff8ed74141b79ef9dc86b30baddf1916c4f
tristen s. <78326803+BopeChanUwU@users.noreply.github.com>
2025-10-13 22:57:25 -0700

    feat: add SaveButton component with styles and integrate into settings page

---
+++ src/app/Components/save-button/save-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-save-button',
+  imports: [],
+  templateUrl: './save-button.html',
+  styleUrl: './save-button.css'
+})
+export class SaveButton {
+
+}
--- src/app/profile/settings/settings.ts
+++ src/app/profile/settings/settings.ts
@@ -8,10 +8,12 @@ import { MatFormField } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatLabel } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { DragNDropBox } from "../../Components/drag-n-drop-box/drag-n-drop-box";
+import { SaveButton } from "../../Components/save-button/save-button";
 
 @Component({
   selector: 'app-settings',
-  imports: [Header, MatCardModule, MatIconModule, MatToolbarModule, Sidebar, MatFormField, MatInputModule, MatLabel, MatButtonModule],
+  imports: [Header, MatCardModule, MatIconModule, MatToolbarModule, Sidebar, MatFormField, MatInputModule, MatLabel, MatButtonModule, DragNDropBox, SaveButton],
   templateUrl: './settings.html',
   styleUrl: './settings.css'
 })
