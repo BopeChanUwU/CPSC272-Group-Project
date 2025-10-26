762ba80171a454cfbb21b63fdd9e779c3079e26a
tristen s. <78326803+BopeChanUwU@users.noreply.github.com>
2025-10-26 12:59:44 -0700

    added tool tips to buttons added host colors to app.css and changed color themeing

--- src/app/Components/menu-button/menu-button.ts
+++ src/app/Components/menu-button/menu-button.ts
@@ -2,10 +2,11 @@ import { Component, ViewChild} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { RouterLink } from "@angular/router";
+import { MatTooltip } from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-menu-button',
-  imports: [MatMenuModule, MatButtonModule, RouterLink],
+  imports: [MatMenuModule, MatButtonModule, RouterLink, MatTooltip],
   templateUrl: './menu-button.html',
   styleUrl: './menu-button.css'
 })
