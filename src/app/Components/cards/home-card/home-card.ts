04c2acb96339a0dbace7dd4802cae590de1bbba2
tristen s. <78326803+BopeChanUwU@users.noreply.github.com>
2025-11-15 15:58:13 -0800

    more homecard changes

--- src/app/Components/cards/home-card/home-card.ts
+++ src/app/Components/cards/home-card/home-card.ts
@@ -1,3 +1,4 @@
+// home-card.ts - Receive plain object, not signal
 import { Component, Input, signal } from '@angular/core';
 import { MatCardModule } from "@angular/material/card";
 import { CommonModule } from '@angular/common';
@@ -14,28 +15,28 @@ export class HomeCard {
   @Input() isSkipped: boolean = false;
   @Input() resetAnimation: boolean = false;
 
-  // Recipe data properties
-  @Input() recipe = signal<any>(null);
+  // CHANGED: Accept plain object instead of signal
+  @Input() recipe: any = null;
+  
   defaultImage = 'https://www.shutterstock.com/image-vector/image-not-found-failure-network-260nw-2330163829.jpg';
 
   get displayImageSrc(): string {
-    if (this.recipe().image_url && this.recipe().image_url.startsWith('data:image')) {
-      return this.recipe().image_url;
-    } else if (this.recipe().image_url) {
-      return this.recipe().image_url;
+    if (this.recipe?.image_url && this.recipe.image_url.startsWith('data:image')) {
+      return this.recipe.image_url;
+    } else if (this.recipe?.image_url) {
+      return this.recipe.image_url;
     } else {
       return this.defaultImage;
     }
   }
 
   onImageError() {
-    this.recipe.update((current) => ({
-      ...current,
-      image_url: this.defaultImage,
-    }));
+    if (this.recipe) {
+      this.recipe.image_url = this.defaultImage;
+    }
   }
 
   ngOnChanges() {
-    console.log('Recipe updated in HomeCard:', this.recipe());
+    console.log('Recipe updated in HomeCard:', this.recipe);
   }
 }
